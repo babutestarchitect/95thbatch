c17f006883fbd9624f464521c47df524d6c86eca
prasannabalaji-dev <prasannawonder8@gmail.com>
2026-05-16 16:11:52 +0530

    File comitted

---
+++ NewAutomation1/src/Training1/Training1.java
@@ -0,0 +1,12 @@
+package Training1;
+
+public class Training1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This a training project");
+		
+	}
+
+}
