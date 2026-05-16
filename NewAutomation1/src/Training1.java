d9a13109531feec470823ece0871076288b247c8
prasannabalaji-dev <prasannawonder8@gmail.com>
2026-05-16 17:13:33 +0530

    comitted

--- NewAutomation1/src/Training1.java
+++ NewAutomation1/src/Training1.java
@@ -4,6 +4,7 @@ public class Training1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This a training session");
+System.out.println("Welcome to automation GIT HUB training");
 	}
 
 }
